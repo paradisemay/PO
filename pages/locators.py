ab69e88ef4e17bd58d4775747df4c370047d4dfc
abrgzej <dantumonskiy@gmail.com>
2020-07-22 17:12:41 +0300

    Add new methods to base_page.py

--- pages/locators.py
+++ pages/locators.py
@@ -20,10 +20,12 @@ class ProductPageLocators():
     BASKET_BUTTON = (By.CLASS_NAME, 'btn-add-to-basket')
     LINKS = [
         'http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear',
-        'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
+        'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019',
+        'http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/'
     ]
-    LINK = LINKS[1]
+    LINK = LINKS[2]
     NAME_SELECTOR = (By.CSS_SELECTOR, 'div.col-sm-6.product_main > h1')
     PRICE_SELECTOR = (By.CSS_SELECTOR, 'div.col-sm-6.product_main > p.price_color')
     AFTER_NAME_SELECTOR = (By.CSS_SELECTOR, 'div.alertinner > strong')
     AFTER_PRICE_SELECTOR = (By.CSS_SELECTOR, 'div.alertinner > p > strong')
+    SUCCESSFUL_MESSAGE = (By.CSS_SELECTOR, 'div.alertinner')
--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,12 @@
+import time
+
 import pytest
 
+from .pages.locators import ProductPageLocators
 from .pages.product_page import ProductPage
 
 
+@pytest.mark.skip()
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
@@ -20,3 +24,36 @@ def test_guest_can_go_to_login_page(browser, link):
                        link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()  # открываем страницу
     page.can_add_to_basket()  # можно ли добавить в корзину
+
+
+@pytest.mark.skip()
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, ProductPageLocators.LINK)
+    page.open()
+    page.click_to_basket_button()
+    before = time.time()
+    assert page.is_not_element_present(*ProductPageLocators.SUCCESSFUL_MESSAGE, timeout=4)
+    after = time.time()
+    print(f'#1 {after - before} s')
+
+
+@pytest.mark.skip()
+def test_guest_cant_see_success_message(browser):
+    page = ProductPage(browser, ProductPageLocators.LINK)
+    page.open()
+    # page.click_to_basket_button()
+    before = time.time()
+    assert page.is_not_element_present(*ProductPageLocators.SUCCESSFUL_MESSAGE, timeout=4)
+    after = time.time()
+    print(f'#2 {after - before} s')
+
+
+@pytest.mark.skip()
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, ProductPageLocators.LINK)
+    page.open()
+    page.click_to_basket_button()
+    before = time.time()
+    assert page.is_disappeared(*ProductPageLocators.SUCCESSFUL_MESSAGE, timeout=4)
+    after = time.time()
+    print(f'#3 {after - before} s')
